8d6ab4432eb40032a07ad74644852b461849db3d
Dmitrii <dtiuterev@rambler.ru>
2022-10-16 18:22:08 +0300

    Added ex_14

---
+++ ex_14.py
@@ -0,0 +1,16 @@
+#14. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+number_str = input('Введите число, целое/дробное (разделитель дробной части . ), я выведу сумму его цифр: ')
+if "," in number_str:
+     print('Вы ввели некорректный разделитель дробной части')
+
+number_float = float(number_str)
+if number_float < 0:
+    number_float = -number_float
+number_str = str(number_float)
+
+sum_digits = 0
+for i in number_str:
+    if i != '.':
+        sum_digits += int(i)
+print(f'Сумма цифр = {sum_digits}')
+
